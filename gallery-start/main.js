42c2c1f9c70021a822b2410f07cd88c17e313ce4
maylin155 <maylinlattmoe123@gmail.com>
2024-07-02 16:35:05 +0800

    upload code acdademy projects

---
+++ gallery-start/main.js
@@ -0,0 +1,37 @@
+const displayedImg = document.querySelector('.displayed-img');
+const thumbBar = document.querySelector(".thumb-bar");
+const btn = document.querySelector('button');
+const overlay = document.querySelector('.overlay');
+
+const images = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
+const alts = {
+    'pic1.jpg' : 'Eye',
+    'pic2.jpg' :  'Wave',
+    'pic3.jpg' : 'Purple flowers',
+    'pic4.jpg' : 'Tomb',
+    'pic5.jpg' : 'Moth'
+};
+
+for (const image of images){
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', `images/${image}`);
+    newImage.setAttribute('alt', `alts[${image}]`);
+    thumbBar.appendChild(newImage);
+    newImage.addEventListener("click", e => {
+        displayedImg.src = e.target.src;
+        displayedImg.alt = e.target.alt;
+    });   
+}
+
+btn.addEventListener("click", () => {
+    const btnClass = btn.getAttribute("class");
+    if (btnClass === "dark"){
+        btn.setAttribute("class", "light");
+        btn.textContent = "Lighten";
+        overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+    } else {
+        btn.setAttribute("class", "dark");
+        btn.textContent = "Darken";
+        overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+    }
+});
